fa7efc8cf45e7d0abc76102bb2cf8c849162acc0
wangshuheng07 <146395275+wangshuheng07@users.noreply.github.com>
2024-07-19 01:23:55 -0400

    Delete a8q3d-codewords directory

--- a8q3d-codewords/modules/nato.h
+++
@@ -1,7 +0,0 @@
-// This module provides the NATO phonetic alphabet in SOS format
-
-#include "sos.h"
-
-// build_nato_codebook() provides a the NATO codebook
-// effects: allocates memory (caller must call sos_destroy)
-struct sos *build_nato_codebook(void);
